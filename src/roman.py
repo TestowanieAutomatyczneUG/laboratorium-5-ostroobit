a49c5283c2c847e86ad9ea1158112e7bf41a36dd
Piotr Ostrowski <postrowski.00@gmail.com>
2020-11-02 21:33:57 +0100

    Refactoring code

--- src/roman.py
+++ src/roman.py
@@ -1,36 +1,11 @@
 def roman(num):
+    numbers = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ]
+    symbols = [ "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
     result = ""
-    if num <= 10:
-        if num <= 3:
-           result = "I" * num
-        elif num == 4:
-            result = "IV"
-        elif num <= 8:
-            result = "V" + "I" * (num - 5)
-        else:
-            result = "I" * (10 - num) + "X"
-        return result
-    elif num == 27:
-        return "XXVII"
-    elif num == 48:
-        return "XLVIII"
-    elif num == 49:
-        return "XLIX"
-    elif num == 59:
-        return "LIX"
-    elif num == 93:
-        return "XCIII"
-    elif num == 141:
-        return "CXLI"
-    elif num == 163:
-        return "CLXIII"
-    elif num == 402:
-        return "CDII"
-    elif num == 575:
-        return "DLXXV"
-    elif num == 911:
-        return "CMXI"
-    elif num == 1024:
-        return "MXXIV"
-    elif num == 3000:
-        return "MMM"
+    i = 0
+    while  num > 0:
+        for _ in range(num // numbers[i]):
+            result += symbols[i]
+            num -= numbers[i]
+        i += 1
+    return result
