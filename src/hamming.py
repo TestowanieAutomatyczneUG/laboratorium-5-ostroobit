29e9e1e5cdd97b929a5d293f0f94d616d04b9c2d
Piotr Ostrowski <postrowski.00@gmail.com>
2020-11-02 17:34:04 +0100

    Implementation for disallow second strand longer

--- src/hamming.py
+++ src/hamming.py
@@ -2,7 +2,9 @@ def distance(str1, str2):
     if str1 == "" and str2 == "":
         return 0
     elif len(str1) > len(str2):
-        raise ValueError("Strands' length must be equal!")
+        raise ValueError("Strands' lengths must be equal!")
+    elif len(str2) > len(str1):
+        raise ValueError("Strands' lengths must be longer")    
     else:
         result = 0
         for x in range(len(str1)):
