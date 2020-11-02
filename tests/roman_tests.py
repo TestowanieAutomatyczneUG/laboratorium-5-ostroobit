fac6140cc1c73e1122e61425e46c4a48188911a4
Piotr Ostrowski <postrowski.00@gmail.com>
2020-11-02 20:46:10 +0100

    Test 402

--- tests/roman_tests.py
+++ tests/roman_tests.py
@@ -44,7 +44,7 @@ class RomanNumeralsTest(unittest.TestCase):
 
     def test_60_being_50_10_is_lx(self):
         self.assertEqual(roman(163), "CLXIII")
-    @unittest.skip("Not implemented yet")
+
     def test_400_being_500_100_is_cd(self):
         self.assertEqual(roman(402), "CDII")
     @unittest.skip("Not implemented yet")
