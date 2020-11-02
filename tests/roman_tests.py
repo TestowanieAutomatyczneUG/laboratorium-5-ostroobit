f5bf0131955002c362eb931ffc5fcbdf03a6d400
Piotr Ostrowski <postrowski.00@gmail.com>
2020-11-02 20:47:45 +0100

    Test 911

--- tests/roman_tests.py
+++ tests/roman_tests.py
@@ -50,7 +50,7 @@ class RomanNumeralsTest(unittest.TestCase):
 
     def test_500_is_a_single_d(self):
         self.assertEqual(roman(575), "DLXXV")
-    @unittest.skip("Not implemented yet")
+
     def test_900_being_1000_100_is_cm(self):
         self.assertEqual(roman(911), "CMXI")
     @unittest.skip("Not implemented yet")
