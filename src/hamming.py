773f498faf5b9a592b7e2fc46e813cf31986898c
Piotr Ostrowski <postrowski.00@gmail.com>
2020-11-02 17:24:53 +0100

    Test for long different strands, refactorisation of implementations

--- src/hamming.py
+++ src/hamming.py
@@ -1,9 +1,9 @@
 def distance(str1, str2):
     if str1 == "" and str2 == "":
         return 0
-    elif str1[0] == str2[0] and len(str1) == 1 and len(str2) == 1:
-        return 0
-    elif str1[0] != str2[0] and len(str1) == 1 and len(str2) == 1:
-        return 1
-    elif str1 == str2:
-        return 0
+    else:
+        result = 0
+        for x in range(len(str1)):
+            if str1[x] != str2[x]:
+                result += 1
+        return result
