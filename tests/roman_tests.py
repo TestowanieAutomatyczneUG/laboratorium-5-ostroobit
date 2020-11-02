609478eb9a029d514cb8827afa312bc2af110aed
Piotr Ostrowski <postrowski.00@gmail.com>
2020-11-02 20:50:12 +0100

    Test 1024

--- tests/roman_tests.py
+++ tests/roman_tests.py
@@ -53,7 +53,7 @@ class RomanNumeralsTest(unittest.TestCase):
 
     def test_900_being_1000_100_is_cm(self):
         self.assertEqual(roman(911), "CMXI")
-    @unittest.skip("Not implemented yet")
+
     def test_1000_is_a_single_m(self):
         self.assertEqual(roman(1024), "MXXIV")
     @unittest.skip("Not implemented yet")
