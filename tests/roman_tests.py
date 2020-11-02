8bcaa175b7850768dc3f60377e26b2737cbff746
Piotr Ostrowski <postrowski.00@gmail.com>
2020-11-02 20:46:58 +0100

    Test 575

--- tests/roman_tests.py
+++ tests/roman_tests.py
@@ -47,7 +47,7 @@ class RomanNumeralsTest(unittest.TestCase):
 
     def test_400_being_500_100_is_cd(self):
         self.assertEqual(roman(402), "CDII")
-    @unittest.skip("Not implemented yet")
+
     def test_500_is_a_single_d(self):
         self.assertEqual(roman(575), "DLXXV")
     @unittest.skip("Not implemented yet")
