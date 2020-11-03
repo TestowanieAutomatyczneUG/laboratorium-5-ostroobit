27f9255a937111e5e25c719c981e08ec87f4d4f8
Piotr Ostrowski <postrowski.00@gmail.com>
2020-11-03 22:32:19 +0100

    Test for get verse 2

--- tests/song_tests.py
+++ tests/song_tests.py
@@ -9,6 +9,9 @@ class SongTest(unittest.TestCase):
     def test_verse_1(self):
         self.assertEqual(self.temp.get_verse(1), "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.")
 
+    def test_verse_2(self):
+        self.assertEqual(Song.get_verse(2), "On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.")
+
 
     def tearDown(self):
         self.temp = None
