ee9df48e785c7dbf817e9bb242a170fb307fa3b2
Piotr Ostrowski <postrowski.00@gmail.com>
2020-11-03 22:29:10 +0100

    Code for get verse 1

--- src/song.py
+++ src/song.py
@@ -6,3 +6,6 @@ class Song:
                 if line != "\n":
                     self.text.append(line[:-1])
     
+    def get_verse(self, index):
+        if index == 1:
+            return self.text[0]
--- tests/song_tests.py
+++ tests/song_tests.py
@@ -4,3 +4,5 @@ from src.song import Song
 class SongTest(unittest.TestCase):
     def test_verse_1(self):
         self.assertEqual(Song.get_verse(1), "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.")
+
+    
